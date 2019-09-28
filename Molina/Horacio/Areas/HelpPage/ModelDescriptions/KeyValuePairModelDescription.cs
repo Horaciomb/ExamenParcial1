3d069ff9b39619439698a1630432ff734dabc336
visual_community@outlook.com <visual_community@outlook.com>
2019-09-28 11:58:08 -0400

    Primer Comit jeje

---
+++ Molina/Horacio/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Horacio.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ Molina/Horacio/Models/Clown.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Horacio.Models
+{
+    public enum Lista
+    {
+        Hernan,
+        Hugo,
+        Helga,
+    }
+    public class Clown
+    {
+        [Key]
+        public int ClownID { get; set; }
+        [Required]
+        [Display(Name = "Nombre Completo")]
+        [StringLength(2, MinimumLength = 24)]
+        public string NickName { get; set; }
+        [Required]
+        public Lista Tricks { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Display(Name = "Cumpleaños")]
+        public DateTime Birthdate { get; set; }
+    }
+}
