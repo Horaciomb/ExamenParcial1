c9371a63f432718cf54f9d635ad8f51704581818
visual_community@outlook.com <visual_community@outlook.com>
2019-09-28 12:09:16 -0400

    Comit de emrgencia porque me asuste jejejej

---
+++ Molina/Horacio/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Horacio.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Horacio.Models.Clown> Clowns { get; set; }
+    }
+}
